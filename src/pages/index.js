1918b4c157359fade0a1279de495b22f9dbef5ef
julianjca <julianjca11@gmail.com>
2019-02-12 20:26:15 +0700

    changing favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ const IndexPage = () => (
         <meta charSet="utf-8" />
         <title>Julian Anderson - Web Developer</title>
         <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Open+Sans:300,400,500,600,700,800,900 " rel="stylesheet" />
+
       </Helmet>
       <SEO title="Home" keywords={['react developer', 'software engineer', 'front end web developer', 'web developer']} />
       <h1>
