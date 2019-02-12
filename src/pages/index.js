9d6fb7c3d5c58c7843e90d344e3ae6781d783986
julianjca <julianjca11@gmail.com>
2019-02-12 20:16:06 +0700

    creating home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Helmet } from 'react-helmet';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import SEO from '../components/seo';
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div className="layout-wrapper">
+    <div className="layout-page">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Julian Anderson - Web Developer</title>
+        <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Open+Sans:300,400,500,600,700,800,900 " rel="stylesheet" />
+      </Helmet>
+      <SEO title="Home" keywords={['react developer', 'software engineer', 'front end web developer', 'web developer']} />
+      <h1>
+        Julian Christian Anderson
+      </h1>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </div>
+
 )
 
 export default IndexPage
