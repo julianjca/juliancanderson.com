24634f4a2b12f70dd7d28f9084ff825ae441faab
Julian Anderson <julianjca11@gmail.com>
2020-05-24 10:44:57 +0700

    change fathom analytics script

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -61,14 +61,14 @@ export const Layout = ({ children }) => {
           <script type="text/javascript">
             {`
             (function(f, a, t, h, o, m){
-                a[h] = a[h] || function () {
-                  (a[h].q = a[h].q || []).push(arguments)
-                };
+              a[h]=a[h]||function(){
+                (a[h].q=a[h].q||[]).push(arguments)
+              };
               o=f.createElement('script'),
               m=f.getElementsByTagName('script')[0];
               o.async=1; o.src=t; o.id='fathom-script';
               m.parentNode.insertBefore(o,m)
-            })(document, window, '//stats.juliancanderson.com/tracker.js', 'fathom');
+            })(document, window, '//analytics.juliancanderson.com/tracker.js', 'fathom');
             fathom('set', 'siteId', 'RYLRU');
             fathom('trackPageview');
           `}
