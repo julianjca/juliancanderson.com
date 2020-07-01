7199b0eaac0c321555a848fcb6e0fc23b240cbc1
Julian <julian@jumpcut.com>
2020-07-01 07:26:50 +0700

    fixing links page

--- src/pages/links.js
+++ src/pages/links.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { setConfig } from 'react-hot-loader'
-import PropTypes from 'prop-types'
 
 import { Layout, Links } from '@components'
-import { graphql } from 'gatsby'
 
 //github.com/gatsbyjs/gatsby/issues/9489
 setConfig({ pureSFC: true })
@@ -30,8 +28,9 @@ const defaultLinks = [
     url: 'https://discord.gg/kkXygrs',
   },
 ]
-const LinksPage = ({ data }) => {
-  const links = data ? data.cms.links : defaultLinks
+
+const LinksPage = () => {
+  const links = defaultLinks
   return (
     <Layout isLightTheme>
       <Links links={links} />
@@ -39,33 +38,4 @@ const LinksPage = ({ data }) => {
   )
 }
 
-LinksPage.propTypes = {
-  data: PropTypes.shape({
-    cms: PropTypes.shape({
-      links: PropTypes.oneOfType([
-        PropTypes.shape({
-          text: PropTypes.string,
-          url: PropTypes.string,
-        }),
-        PropTypes.arrayOf(
-          PropTypes.shape({
-            text: PropTypes.string,
-            url: PropTypes.string,
-          })
-        ),
-      ]),
-    }),
-  }),
-}
-
-export const query = graphql`
-  query {
-    cms {
-      links {
-        text
-        url
-      }
-    }
-  }
-`
 export default LinksPage
