32e13adb03712a468465a05733a91a768b94e042
Julian Anderson <julian@jumpcut.com>
2021-02-05 10:43:11 +0700

    removing getStaticPath

--- src/pages/bookshelf.js
+++ src/pages/bookshelf.js
@@ -1,7 +1,7 @@
 // Install remark and remark-html
 import remark from 'remark'
 import html from 'remark-html'
-import { getPostBySlug, getAllPosts } from '../lib/blog'
+import { getPostBySlug } from '../lib/blog'
 
 import BlogPost from '../templates/BlogPost'
 
@@ -19,20 +19,5 @@ export const getStaticProps = async ({ params }) => {
       ...post,
       content,
     },
-  }
-}
-
-export const getStaticPaths = async () => {
-  const posts = getAllPosts()
-
-  return {
-    paths: posts.map(post => {
-      return {
-        params: {
-          slug: post.slug,
-        },
-      }
-    }),
-    fallback: false,
   }
 }
